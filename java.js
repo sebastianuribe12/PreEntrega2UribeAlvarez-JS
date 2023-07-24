79289687dd0de88ed5cdcfb1c7d6a4de70e0c6c7
sebastianuribe12 <urisbe@gmail.com>
2023-07-24 12:23:55 -0500

    Cambios en los ingresos del usuario para la actividad

--- java.js
+++ java.js
@@ -7,13 +7,27 @@ if(Number.isNaN(NumPer) == true){
         NumPer = parseInt(prompt("cuantas personas se hospedaran"));
     }
 }
-let Regdia;//Registro de dias
-let n;//ciclo
-
-
-    AcumEd = AcumEd+RegEd;
-    RegEd = NaN;
-    
+let Regdia = parseInt(prompt("Cuantos dias se hospedaran"));//Registro de dias
+if(Number.isNaN(Regdia) == true){
+    while(Number.isNaN(Regdia) == true){
+        alert("Porfavor ingrese algun valor Valido");
+        Regdia = parseInt(prompt("cuantos dias estaran hospedados"));
+    }
 }
+
+let aux; //auxiliar para las habitaciones\
+let NumH;
+
+if( NumPer >= 4){
+    if(NumPer %4 == 0){
+        NumH = NumPer/4;
+    }
+    else{
+        aux = NumPer/4;
+        NumH =
+    }
+}
+    
+
 console.log(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
-alert(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
+alert(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`); 
