20cddd605266d6ecacd6090c674a910af6b0cdf4
sebastianuribe12 <urisbe@gmail.com>
2023-07-24 12:37:36 -0500

    Se agregan lineas luego de hacer un debug

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
-alert("Simulador Valor Hotel ✌️\n -----------por Sebastian Uribe Alvarez-----------");
+alert("Simulador Valor Hotel 🏨✌️\n -----------por Sebastian Uribe Alvarez-----------");
 
-let NumPer = parseInt(prompt("cuantas personas Se hospedaran \n ( las habitaciones se dividiran en grupos de 4 personas)"));//numero de personas 
+let NumPer = parseInt(prompt("cuantas personas Se hospedaran  \n ( las habitaciones se dividiran en grupos de 4 personas👨‍👩‍👧‍👦)"));//numero de personas 
 if(Number.isNaN(NumPer) == true){
     while(Number.isNaN(NumPer) == true){
         alert("Porfavor ingrese algun valor Valido");
@@ -25,9 +25,12 @@ if( NumPer >= 4){
     else{
         aux = NumPer/4;
         NumH = (aux)-((NumPer%4)/4);
+        NumH = NumH+1;
     }
 }
-    
+else{
+    NumH =1;
+}    
 
-console.log(`El valor de hospedaje para ${NumPer} personas en ${NumH} Habitaciones  en ${Regdia} dias es : ${(NumH*Regdia)*cost}`);
-alert(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`); 
+console.log(`El valor de hospedaje para ${NumPer} personas en ${NumH} Habitaciones  en ${Regdia} dias es : ${(NumH*Regdia)*cost} usd`);
+alert(`El valor de hospedaje para ${NumPer} personas en ${NumH} Habitaciones  en ${Regdia} dias es : ${(NumH*Regdia)*cost} usd`);  
